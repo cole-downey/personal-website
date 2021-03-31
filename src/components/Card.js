ba73da373c4163770fd6f99eb4107849ccd41e0b
Cole Downey <coledowney@tamu.edu>
2021-03-31 04:22:38 -0500

    added support for video cards and updated porfolio

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Card, CardActionArea, CardContent, Grid, SvgIcon } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, CardMedia, Grid, SvgIcon } from '@material-ui/core';
 import styled from 'styled-components';
 import { CardText } from './Typography';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player'
 
-const StyledCard = styled(Card)`
+export const StyledCard = styled(Card)`
     background-color: transparent;
     border: 2px solid white;
     border-radius: 0;
     padding: 1em;
     padding-left: 2em;
+    height: 100%
+`
+const StyledVideoCard = styled(Card)`
+    background-color: transparent;
+    border: 2px solid white;
+    border-radius: 0;
+    padding: 1em;
     height: 100%
 `
 
@@ -43,6 +51,42 @@ export const ClickableCard = (props) => {
                     </Grid>
                 </CardContent>
             </StyledCard>
+        </StyledCardActionArea>
+    )
+}
+
+export const VideoCard = (props) => {
+    const [icon, title, description, url, vidUrl] = [props.icon, props.title, props.description, props.url, props.vidUrl];
+    return (
+        <StyledCardActionArea href={url} to={url} {... (props.useLink ? { component: Link } : {})}>
+            <StyledVideoCard >
+                <CardContent >
+                    <Grid container alignItems="stretch" style={{ height: "100%" }}>
+                        <Grid container spacing="6" alignItems="center">
+                            <Grid item>
+                                {props.notMaterialIcon ? <StyledIcon>{icon}</StyledIcon> : <StyledIcon component={icon} />}
+                            </Grid>
+                            <Grid item xs={12} sm>
+                                <CardText variant="h6" gutterBottom>
+                                    {title}
+                                </CardText>
+                                <CardText>
+                                    {description}
+                                </CardText>
+                            </Grid>
+
+                        </Grid>
+                    </Grid>
+                </CardContent>
+                <CardMedia>
+                        <ReactPlayer
+                            controls
+                            width={"fluid"}
+                            height={"480px"}
+                            url={vidUrl}
+                        />
+                </CardMedia>
+            </StyledVideoCard>
         </StyledCardActionArea>
     )
 }
--- src/containers/portfolio/Portfolio.js
+++ src/containers/portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import { BodyText } from '../../components/Typography';
-import { ClickableCard } from '../../components/Card';
+import { ClickableCard, VideoCard } from '../../components/Card';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
@@ -39,11 +39,35 @@ const Portfolio = () => {
                     Some highlights are shown below:
                 </BodyText>
             </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+                <VideoCard title="OpenGL A4 - Free-Look World"
+                    description="In this project, I created a 3d world and a free-look camera that responds to WASD and mouse movement, 
+                    as well as a HUD and second, top-down orthogonal camera for a mini-map"
+                    icon={GitHubIcon}
+                    url="https://github.com/cole-downey/441-A4-free-look-camera"
+                    vidUrl="https://youtu.be/nR7hdqTI1Kc" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+                <VideoCard title="OpenGL A3 - Shaders"
+                    description="In this project, I wrote several vertex and fragment shaders, including a Blinn-Phong shader, 
+                    a silhouette shader, and a cel shader, all of which support multiple lights and materials"
+                    icon={GitHubIcon}
+                    url="https://github.com/cole-downey/441-A3-shaders"
+                    vidUrl="https://youtu.be/sRDT_WFnJis" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+                <VideoCard title="OpenGL A2 - Hierarchical Transforms"
+                    description="In this project, I created a hierarchical robot figure with components that respond to 
+                    keyboard inputs to change their rotations"
+                    icon={GitHubIcon}
+                    url="https://github.com/cole-downey/441-A2-hierarchical-transforms"
+                    vidUrl="https://youtu.be/8a-tZvqdg9s" />
+            </Grid>
             <Grid item xs={12}>
-                <ClickableCard title={"Moodia"} 
-                description={"Final project for CSCE 315. This is the biggest project I've worked on this far, with 4 other people. Moodia is a web service allowing users to view a customized Twitter timeline, with the ability to filter out Tweets based on the mood they portray. Created with React, using the Twitter, Spotify, and Google Sentiment Analysis APIs. This is a link to the deployed app. Note: we are in the process of renewing our Google Sentiment Analysis subscription, so the mood filters and the CSS may be broken currently on Moodia."}
-                icon={EmojiEmotionsIcon} 
-                url={"https://moodia-315.herokuapp.com/"} />
+                <ClickableCard title={"Moodia"}
+                    description={"Final project for CSCE 315. This is the biggest project I've worked on this far, with 4 other people. Moodia is a web service allowing users to view a customized Twitter timeline, with the ability to filter out Tweets based on the mood they portray. Created with React, using the Twitter, Spotify, and Google Sentiment Analysis APIs. This is a link to the deployed app. Note: we are in the process of renewing our Google Sentiment Analysis subscription, so the mood filters and the CSS may be broken currently on Moodia."}
+                    icon={EmojiEmotionsIcon}
+                    url={"https://moodia-315.herokuapp.com/"} />
             </Grid>
             {portItems.map((item, key) => (
                 <Grid item xs={12} sm={12} md={6} lg={4}>
