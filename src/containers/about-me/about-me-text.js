6af5ec2cf4d738ed4f64eff570e6aa181e6f35bd
Cole Downey <cdowneymm@gmail.com>
2021-08-23 20:00:00 -0500

    senior year, baby!

--- src/containers/about-me/about-me-text.js
+++ src/containers/about-me/about-me-text.js
@@ -1,6 +1,6 @@
 // I will def structure text blocks better later
 const AboutMeText = [
-    "My name is Cole Downey, and I am a junior at Texas A&M, class of ’22. I'm pursuing a Computer Science major and a Video Game Development and Design minor. My favorite hobbies are longboarding, skiing, playing music (ukulele and piano), and playing/creating video games.",
+    "My name is Cole Downey, and I am a senior at Texas A&M, class of ’22. I'm pursuing a Computer Science major and a Video Game Development and Design minor. My favorite hobbies are longboarding, skiing, playing music (ukulele and piano), and playing/creating video games.",
     "After graduating, I want to pursue a career in either Game Development or Software Development.",
 ];
 
