567a9439a3af319d0004d30f48aac0ba8ba1c3d1
Cole Downey <cdowneymm@gmail.com>
2021-11-15 16:30:46 -0600

    updated with animation projeects

--- src/containers/portfolio/Portfolio.js
+++ src/containers/portfolio/Portfolio.js
@@ -8,6 +8,12 @@ import PublicIcon from '@material-ui/icons/Public';
 
 const vidItems = [
     {
+        title: "Animation P5: Cloth Simulation",
+        description: "In this project, I created a realistic cloth using physics based animation. I represented the cloth as a system of particles stored in various matrices and vectors, and did implicit Euler integration to solve for the new position of each particle at every step. Since I used implicit integration, the simulation remains stable even when using larger timesteps. Additionally, all physics matrices were stored and used as sparse matrices, which significantly increased performance. Created in C++ using OpenGL.",
+        url: "https://github.com/cole-downey/a5-cloth-simulation",
+        vidUrl: "https://youtu.be/wBgUl0rzGXk",
+    },
+    {
         title: "Unity P3: Spaceboarder",
         description: "This is the game I'm working on now: a 3D snowboarding game with procedurally generated terrain that wraps around into a tube. I have a dedicated page for this project, as I'm planning on working on it over a long period of time with and logging my updates. Click this card to go to the Devlog.",
         url: "/portfolio/spaceboarder",
@@ -22,16 +28,28 @@ const vidItems = [
         vidUrl: "https://youtu.be/5O-_e5ZfSpI",
     },
     {
-        title: "Unity Project 1",
-        description: "This is my first major unity project that I've made myself. I haven't thought of a good name yet, but I'm aiming for it to play like a 3D platformer roguelike. The main mechanics I've worked on so far are the platform/ block spawning, the player movement control, and enemy behavior.",
-        url: "https://github.com/cole-downey/first-unity-project",
-        vidUrl: "https://youtu.be/IAf3S_vx5vk",
-    },
-    {
         title: "OpenGL A5 - Deferred Rendering",
         description: "In this project, I implemented deferred rendering into 4 textures: position, normal, ka, and kd. Additionally, I created dynamic geometry calculated on the GPU, procedurally generated spheres, and support for multiple lights in the shaders. Written in C++ using OpenGL.",
         url: "https://github.com/cole-downey/441-A5-deferred-rendering",
         vidUrl: "https://youtu.be/Pbyhqm-oYdE",
+    },
+    {
+        title: "Animation P4 - Inverse Kinematics",
+        description: "In this project, I implemented an inverse kinematics controlled chain. I used Gradient Descent with Backtracking Line Search and Newton's Method to iterate towards an optimized solution, with the target being the user's mouse. In this demo the chain has 4 links, but my project supports any number of links. Written in C++ using OpenGL.",
+        url: "https://github.com/cole-downey/a4-inverse-kinematics",
+        vidUrl: "https://youtu.be/FA_N6jq8RzY",
+    },
+    {
+        title: "Animation P3 - Blend Shape Facial Animation",
+        description: "In this project, I took an input of a face mesh and a set of defined facial action units, and implemented blend shape animation. I combined settings of different facial action units to create multiple blend shapes, then interpolated through them on the GPU using a GLSL shader. Created in C++ using OpenGL and Maya.",
+        url: "https://github.com/cole-downey/a3-facial-blend-shapes",
+        vidUrl: "https://youtu.be/MIu0e9HAEB4",
+    },
+    {
+        title: "Unity Project 1",
+        description: "This is my first major unity project that I've made myself. I haven't thought of a good name yet, but I'm aiming for it to play like a 3D platformer roguelike. The main mechanics I've worked on so far are the platform/ block spawning, the player movement control, and enemy behavior.",
+        url: "https://github.com/cole-downey/first-unity-project",
+        vidUrl: "https://youtu.be/IAf3S_vx5vk",
     },
     {
         title: "OpenGL A4 - Free-Look World",
@@ -76,7 +94,7 @@ const Portfolio = () => {
         >
             <Grid item xs={12}>
                 <BodyText variant="h6" paragraph>
-                    Click on each card to see the project
+                    Click on each card to see the project repo
                 </BodyText>
                 <BodyText variant="body1">
                     Some highlights are shown below:
@@ -88,11 +106,11 @@ const Portfolio = () => {
             </Grid>
             {vidItems.map((item, key) => (
                 <Grid item xs={12} sm={12} md={12} lg={(vidItems.length % 2 !== 0 && key === 0) ? 6 : 6}>
-                    <VideoCard title={item.title} description={item.description} 
-                        icon={item.hasOwnProperty('icon')? item.icon : GitHubIcon}
-                        url={item.url} vidUrl={item.vidUrl} key={key} 
+                    <VideoCard title={item.title} description={item.description}
+                        icon={item.hasOwnProperty('icon') ? item.icon : GitHubIcon}
+                        url={item.url} vidUrl={item.vidUrl} key={key}
                         useLink={item.hasOwnProperty('useLink')}
-                        />
+                    />
                 </Grid>
             ))}
             <Grid item xs={12}>
